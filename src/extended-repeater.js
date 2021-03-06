2eaf4c00cdde309c7a0f79a81b175ac0425fdec7
YuliyaTurovskaya <turovskaya.yuliya24@gmail.com>
2021-03-06 12:56:45 +0300

    feat: add solution task simple-chain

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let array = [];
+
+  if(options.repeatTimes === undefined){
+    options.repeatTimes = 1;
+  }
+
+  if(options.additionRepeatTimes === undefined){
+    options.additionRepeatTimes = 1;
+  }
+
+  if(options.separator === undefined){
+    options.separator = '+';
+  }
+
+  if(options.additionSeparator === undefined){
+    options.additionSeparator = '|';
+  }
+
+  if(options.addition === undefined){
+    options.addition = '';
+  }
+
+  for (i=0; i<options.repeatTimes; i++){
+
+    if(options.repeatTimes > 1 || options.repeatTimes === 1){
+      array.push(String(str));
+
+      for(j=0; j<options.additionRepeatTimes; j++){
+      (options.additionRepeatTimes > 1 && j != options.additionRepeatTimes-1) ?
+      array.push(String(options.addition), options.additionSeparator) :
+      array.push( String(options.addition));
+      }
+    }
+
+    if(options.repeatTimes > 1 && i != options.repeatTimes-1){
+      array.push(options.separator);
+    }
+  }
+
+  return array.join('');
 };
-  
