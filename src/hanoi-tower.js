92f81b4623aeb25ed0993519bb248bb4a93a951d
YuliyaTurovskaya <turovskaya.yuliya24@gmail.com>
2021-03-06 10:57:13 +0300

    fix: solution task hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,6 +3,6 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   return {
     turns: Math.pow(2, disksNumber) -1,
-    seconds: Math.floor(turnsNumber/turnsSpeed*3600)
+    seconds: Math.floor((Math.pow(2, disksNumber) -1)/turnsSpeed*3600)
   }
 };
