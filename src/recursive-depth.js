7aa77edc33a968bf4117db562be67eed857488c6
YuliyaTurovskaya <turovskaya.yuliya24@gmail.com>
2021-03-07 21:59:26 +0300

    feat: add solution task recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+
+    let result = 1;
+
+    if(Array.isArray(arr)){
+      let currentResult = 0;
+      for (let item of arr){
+        currentResult =this.calculateDepth(item)+1;
+
+        if(currentResult > result){
+          result = currentResult;
+        }
+      }
+
+    }else{
+      return 0;
+    }
+
+    return result;
   }
-};
+}
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,74 @@
 const CustomError = require("../extensions/custom-error");
 
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(condition) {
+    if (condition === undefined) {
+      this.condition = true;
+    } else {
+      this.condition = condition;
+    }
+  }
+
+  encrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error('Error');
+    }
+
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+
+    let encryptWord = [];
+
+    if(key.length < message.length){
+      for(i=0; key.length < message.length; i++){
+        key.push(key[i]);
+      }
+    }
+
+    for(i=0; i<message.length; i++){
+      if(alphabet.indexOf(message[i]) >= 0){
+        encryptWord.push(alphabet[(alphabet.indexOf(message[i])+alphabet.indexOf(key[i])) % 26]);
+      } else{
+        encryptWord.push(message[i]);
+        key.splice(i, 0, ' ');
+      }
+    }
+
+    return this.condition ? encryptWord.join('') : encryptWord.reverse().join('')
+  }
+
+  decrypt(message, key) {
+    if (message == undefined || key == undefined) {
+      throw new Error('Error');
+    }
+
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+
+    let decryptWord = [];
+      if(key.length < message.length){
+        for(i=0; key.length < message.length; i++){
+          key.push(key[i]);
+        }
+      }
+
+    for(i=0; i<message.length; i++){
+
+      if(alphabet.indexOf(message[i]) >= 0){
+        if((alphabet.indexOf(message[i])-alphabet.indexOf(key[i])) < 0){
+          decryptWord.push(alphabet[(alphabet.indexOf(message[i])-alphabet.indexOf(key[i])+26) % 26])
+        } else{
+          decryptWord.push(alphabet[(alphabet.indexOf(message[i])-alphabet.indexOf(key[i])) % 26]);
+        }
+
+      } else{
+        decryptWord.push(message[i]);
+        key.splice(i, 0, ' ');
+      }
+    }
+
+    return this.condition ? decryptWord.join('') : decryptWord.reverse().join('')
   }
 }
 
